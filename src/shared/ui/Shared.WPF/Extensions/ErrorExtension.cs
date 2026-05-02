263f048a2d26fe8324ec915856a24954939cbc5d
texno stalin <texno.stalin@gmail.com>
2026-05-02 15:52:27 +0300

    feat: Добавлен метод расширения для более удобного вывода кода и причин ошибкок

---
+++ src/shared/ui/Shared.WPF/Extensions/ErrorExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Windows;
+using Terminex.Common.Results;
+
+namespace Shared.WPF.Extensions
+{
+    public static class ErrorExtension
+    {
+        public static void ShowError(this IList<Error> errors)
+        {
+            if (errors.Count > 0)
+            {
+                var sb = new StringBuilder();
+
+                foreach (var item in errors)
+                    sb.AppendLine($"ErrorCode: {item.ErrorCode}").AppendLine($"ErrorMessage: {item.Message}");
+
+                MessageBox.Show(sb.ToString());
+            }
+        }
+    }
+}
