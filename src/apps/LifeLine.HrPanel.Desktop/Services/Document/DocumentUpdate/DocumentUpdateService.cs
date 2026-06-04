e9d3be027bd3f032c8b7dfbf9db1c15eb883e31d
texno stalin <texno.stalin@gmail.com>
2026-06-04 14:07:29 +0300

    feat: Был реализован сервис `DocumentUpdateService` для обновленя документов в S3-хранилищи, отправку результата в базу данных, и удаление старого документы из s3-хранилища при успехе добавления

---
+++ src/apps/LifeLine.HrPanel.Desktop/Services/Document/DocumentUpdate/DocumentUpdateService.cs
@@ -0,0 +1,44 @@
+﻿using LifeLine.File.Service.Client;
+using Shared.Contracts.Request.Files;
+using Shared.WPF.Helpers;
+using Terminex.Common.Results;
+
+namespace LifeLine.HrPanel.Desktop.Services.Document.DocumentUpdate
+{
+    public sealed class DocumentUpdateService(IFileStorageService storageService) : IDocumentUpdateService
+    {
+        public async Task<Result> UpdateDocumentFileAsync<TDbRequest>
+            (
+                byte[] newFileBytes,
+                string newFileName,
+                string? oldFileKey,
+                Func<byte[], string, UploadFileRequest> uploadRequest,
+                Func<string, TDbRequest> dbRequest,
+                Func<TDbRequest, Task<Result>> dbUpdateAsync
+            )
+        {
+            var s3Request = uploadRequest(newFileBytes, newFileName);
+            var s3Result = await storageService.UploadFileAsync(s3Request);
+
+            if (s3Result.IsFailure)
+                return Result.Failure(Error.BadRequest(s3Result.StringMessage));
+
+            var s3FileName = s3Result.Value!.FileName;
+
+            var dbReq = dbRequest(s3FileName);
+            var dbResult = await dbUpdateAsync(dbReq);
+
+            if (dbResult.IsFailure)
+                return Result.Failure(Error.BadRequest(dbResult.StringMessage));
+
+            if (!string.IsNullOrWhiteSpace(oldFileKey))
+            {
+                var (bucketName, fileName) = S3UrlParser.Parse(oldFileKey);
+
+                await storageService.DeleteFileAsync(new DeleteFileRequest(bucketName!, fileName!));
+            }
+
+            return Result.Success();
+        }
+    }
+}
